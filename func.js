6be0eed2b01adebd0343243a397d08d4ed461f74
Brian <wl03473869@gmail.com>
2023-03-11 15:05:53 +0800

    20230311_3

--- func.js
+++ func.js
@@ -4,13 +4,16 @@ function gen(){
     let max = parseInt(document.getElementById("max").value);
     let quantity = parseInt(document.getElementById("quantity").value);
     let result = document.getElementById("result");
+    // 清除上一次的結果
     while (child.firstChild){
         child.removeChild(child.firstChild);
     }
+    // 檢查輸入是否正確
     if ( min > max || isNaN(min) || isNaN(max) || isNaN(quantity) ){
         alert('請輸入正確的數字');
         return
     }
+    // 產生隨機數字
     for (let i = 0; i < quantity; i++){
         let res = Math.floor(Math.random()*(max-min+1))+min;
         let div = document.createElement("div");
